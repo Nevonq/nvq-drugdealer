5b4cd923553c0c1e5031947d7d1f5a27f62e27c1
Nevonq <107314027+Nevonq@users.noreply.github.com>
2023-07-23 21:04:17 +0300

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,21 @@
+Locales['en'] = {
+    -- Drug Dealer
+    ['drugdealer'] = 'Drug Dealer',
+    ['currency'] = '€',
+    ['amount'] = 'Amount',
+    ['help'] = 'Press ~INPUT_CONTEXT~ to open the menu.',
+    -- For OkOk Notifications
+    ['you_bought'] = 'You bought: ',
+    ['sold_item'] = 'You sold: ',
+    ['not_enough'] = 'You dont have enough money!',
+    ['not_enoughs'] = 'You dont have enough items!',
+    ['incorrect'] = 'The value is not correct!',
+    -- For ESX
+    ['sold'] = 'You sold an item to the dealer ',
+    ['buy'] = 'You bought an item from the dealer ',
+    ['shownotifyesx'] = 'You dont have enough items!',
+    ['incs'] = 'The value is not correct!',
+    -- Other
+    ['item'] = 'x.',
+  
+}
