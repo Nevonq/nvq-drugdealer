2df85cf88967faf227ac77cec20d27f64ee10920
Nevonq <107314027+Nevonq@users.noreply.github.com>
2023-10-30 20:42:23 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,9 @@ fx_version 'cerulean'
 game 'gta5'
 Author 'Nevonq'
 Description 'Simple drug dealer system that allows you to buy and sell items from the dealer.'
-Version '1.0'
+Version '1.1'
+lua54 'yes'
+
 client_scripts {
     'configs/*.lua',
     '@es_extended/locale.lua',    
@@ -15,4 +17,10 @@ server_scripts {
     '@es_extended/locale.lua',    
     'configs/*.lua',
     'locales/*.lua',
+}
+
+shared_scripts {
+    '@es_extended/imports.lua',
+    '@ox_lib/init.lua',
+    'configs/*.lua',
 }
