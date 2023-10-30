0842b27d1ab46e854f2e2f033ee834787c3a5da3
Nevonq <107314027+Nevonq@users.noreply.github.com>
2023-10-30 20:40:54 +0200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,36 +1,45 @@
-ESX = nil
-isinalr = false
 
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(1200)
-	end
-end)
-if Config.EnablePed then
-	Citizen.CreateThread(function()
-		RequestModel(Config.Ped)
-		while not HasModelLoaded(Config.Ped) do
-			Wait(550)
-		end
-		for k,v in pairs(Config.DrugDealer) do
-			local vectoras = vector3(v.x,v.y,v.z-1.0)
-			ped = CreatePed(4, Config.Ped, vectoras, 330.50, false, true) -- The 120.50 value is the player ped Heading
-		FreezeEntityPosition(ped, true)
-			SetEntityInvincible(ped, true)
-			SetBlockingOfNonTemporaryEvents(ped, true)
-			TaskSetBlockingOfNonTemporaryEvents(ped, true)
-		end
-	end)
+local showText = false
+local markerInRange = false
+local playerPed = PlayerPedId()
+
+if Config.NewESX then 
+	ESX = exports["es_extended"]:getSharedObject()
 else
-	Citizen.Wait(Config.Wait)
+		ESX = nil
+		Citizen.CreateThread(function()
+			while ESX == nil do
+				TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+				Citizen.Wait(1200)
+			end
+		end)
+end
+
+if not Config.EnableOxLib then
+	if Config.EnablePed then
+		Citizen.CreateThread(function()
+			RequestModel(Config.Ped)
+			while not HasModelLoaded(Config.Ped) do
+				Wait(550)
+			end
+			for k,v in pairs(Config.DrugDealer) do
+				local vectoras = vector3(v.x,v.y,v.z-1.0)
+				local ped = CreatePed(4, Config.Ped, vectoras, Config.Heading, false, true) 
+				FreezeEntityPosition(ped, true)
+				SetEntityInvincible(ped, true)
+				SetBlockingOfNonTemporaryEvents(ped, true)
+				TaskSetBlockingOfNonTemporaryEvents(ped, true)
+			end
+		end)
+	else
+		Citizen.Wait(1500)
+	end
 end
 
 
 
 
 function OpenDrugDealerMenu()
-	isinalr = true
     ESX.UI.Menu.Open(
     	'default', GetCurrentResourceName(), 'drug_main',
     	{
@@ -52,56 +61,51 @@ function OpenDrugDealerMenu()
     	end,
     	function(data, menu)
         	menu.close()
-			isinalr = false
     	end
 	)
 end
 
+if not Config.EnableOxLib then
+	Citizen.CreateThread(function()
+		while true do
+			local sleep_optimizations = 1500
+			local coords = GetEntityCoords(playerPed)
+			markerInRange = false
 
-local showText = false
-local markerInRange = false
-local playerPed = PlayerPedId()
+			for k, v in ipairs(Config.DrugDealer) do
+				local distance = #(coords - v)
+				if distance < 5 then
+					sleep_optimizations = 0
+					if Config.Enable3D then
+						DrawText3D(v.x, v.y, v.z + Config.HeightOfTheText, '[E] Drug Dealer')
+						if distance < 2.5 and IsControlJustPressed(0, 38) then
+							OpenDrugDealerMenu()
+						end
+					end
 
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(5)
+					if Config.EnableMarker and not markerInRange then
+						DrawMarker(Config.MarkerId, v.x, v.y, v.z - 0.95, 0, 0, 0, 0, 0, 0, 1.5, 1.5, 0.2, 255, 255, 255, 200, false, true, 2, nil, nil, false)
+						if distance < 2.5 then
+							markerInRange = true
+						end
+					end
+				
+				end
+			end
 
-        local coords = GetEntityCoords(playerPed)
-        showText = false
-        markerInRange = false
-
-        for k, v in ipairs(Config.DrugDealer) do
-            local distance = #(coords - v)
-
-            if Config.Enable3D and distance < 5 and not showText then
-                showText = true
-                DrawText3D(v.x, v.y, v.z + Config.HeightOfTheText, '[E] Drug Dealer')
-                if distance < 2.5 and IsControlJustPressed(0, 38) then
-                    OpenDrugDealerMenu()
-                end
-            end
-
-            if Config.EnableMarker and distance < 5 and not markerInRange then
-                DrawMarker(Config.MarkerId, v.x, v.y, v.z - 0.95, 0, 0, 0, 0, 0, 0, 1.5, 1.5, 0.2, 255, 255, 255, 200, false, true, 2, nil, nil, false)
-                if distance < 1.1 then
-                    markerInRange = true
-                end
-            end
-        end
-
-        if markerInRange then
-            ESX.ShowHelpNotification(_U('help'))
-            if IsControlJustPressed(0, 38) then
-                OpenDrugDealerMenu()
-            end
-        end
-    end
-end)
-
+			if markerInRange then
+				ESX.ShowHelpNotification(_U('help'))
+				if IsControlJustPressed(0, 38) then
+					OpenDrugDealerMenu()
+				end
+			end
+			Wait(sleep_optimizations)
+		end
+	end)
+end
 
 function DrawText3D(x, y, z, text)
 	local onScreen, _x, _y = World3dToScreen2d(x, y, z)
-
 	SetTextScale(0.25, 0.25)
 	SetTextFont(0)
 	SetTextProportional(1)
@@ -116,7 +120,6 @@ end
 
 
 function BuyMenu()
-	isinalr = true
 	local player = PlayerPedId()
 	local elements = {}
 			
@@ -124,7 +127,7 @@ function BuyMenu()
 		if v.BuyInPawnShop then
 			table.insert(elements,
             {
-                label = v.label .. " | "..('<span style="color:red;">%s</span>'):format("â‚¬"..v.BuyPrice..""), 
+                label = v.label .. " | "..('<span style="color:red;">%s</span>'):format(_U('currency')..v.BuyPrice..""), 
                 itemName = v.itemName, 
                 BuyInPawnShop = v.BuyInPawnShop, 
                 BuyPrice = v.BuyPrice
@@ -162,7 +165,6 @@ function BuyMenu()
 	end,
 	function(data, menu)
 		menu.close()
-		isinalr = false
 	end,
 	function(data, menu)
 	end
@@ -170,7 +172,6 @@ function BuyMenu()
 end
 
 function SellMenu()
-	isinalr = true
 	local player = PlayerPedId()
 	local elements = {}
 
@@ -178,7 +179,7 @@ function SellMenu()
 		if v.SellInPawnShop then
 			table.insert(elements,
             {
-                label = v.label .. " | "..('<span style="color:red;">%s</span>'):format("â‚¬"..v.SellPrice..""), 
+                label = v.label .. " | "..('<span style="color:red;">%s</span>'):format(_U('currency')..v.SellPrice..""), 
                 itemName = v.itemName, 
                 SellInPawnShop = v.SellInPawnShop,
                 SellPrice = v.SellPrice
@@ -216,7 +217,6 @@ function SellMenu()
 	end,
 	function(data, menu)
 		menu.close()
-		isinalr = false
 	end,
 	function(data, menu)
 	end
@@ -228,7 +228,6 @@ end
 
 
 -- BLIP START
-
 
 if Config.EnableBlip then
     Citizen.CreateThread(function()
@@ -247,3 +246,58 @@ if Config.EnableBlip then
 end
 
 -- BLIP END
+
+
+-- OX LIB, OX TARGET STUFF.
+if Config.EnableOxLib then
+	local ped = PlayerPedId()
+	local vehicle = cache.vehicle
+
+	lib.onCache('ped', function(value) ped = value end)
+	lib.onCache('vehicle', function(value) vehicle = value end)
+
+	for k, v in pairs(Config.DrugDealerOx) do
+		if v.coords then
+			lib.points.new(v.coords.xyz, 100.0, {
+				onEnter = function(self)
+					if not self.npc then
+						ESX.Streaming.RequestModel(Config.Ped)
+						local npc = CreatePed(4, Config.Ped, v.coords, false, true)
+						FreezeEntityPosition(npc, true)
+						SetEntityInvincible(npc, true)
+						SetBlockingOfNonTemporaryEvents(npc, true)
+						SetModelAsNoLongerNeeded(Config.Ped)
+						self.npc = npc
+			
+						exports.ox_target:addLocalEntity(npc, {
+							{
+								name = 'nvq-drugs:drugs_dealer',
+								icon = 'fa-solid fa-tablets',
+								label = _U('ox_interact'),
+								distance = 2.0,
+								canInteract = function(entity)
+									return entity
+								end,
+								onSelect = function(entity)
+									OpenDrugDealerMenu()
+								end,
+							}
+						})
+					end
+				end,
+				onExit = function(self)
+					
+				end
+			})
+		end
+	end
+end 
+
+
+
+-- For fixing the bug when the ui is frozen when in menu restarting the resource
+AddEventHandler('onResourceStop', function(resource)
+	if resource == GetCurrentResourceName() then
+		ESX.UI.Menu.CloseAll()
+	end
+end)
